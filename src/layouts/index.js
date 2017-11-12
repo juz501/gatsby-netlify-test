bdc89ab7dcabd1c97dcd4f2be7d90cd8e594edd2
Julian Chan <julian.chan@chromatix.com.au>
2017-11-12 23:55:55 +1100

    use arrow syntax

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,18 +1,22 @@
 import React from "react";
+import PropTypes from "prop-types";
 import Helmet from "react-helmet";
 import config from "../../data/SiteConfig";
 import "./global.scss";
 
-export default class MainLayout extends React.Component {
-  render() {
-    const { children } = this.props;
-    return (
-      <div className="main-layout">
-        <Helmet>
-          <meta name="description" content={config.siteDescription} />
-        </Helmet>
-        {children()}
-      </div>
-    );
-  }
+const MainLayout = ({ children }) => {
+  return (
+    <div className="main-layout">
+      <Helmet>
+        <meta name="description" content={config.siteDescription} />
+      </Helmet>
+      <div className="content-container">{children()}</div>
+    </div>
+  )
+};
+
+MainLayout.propTypes = {
+  children: PropTypes.func,
 }
+
+export default MainLayout;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,22 @@
 import React from "react";
 import Helmet from "react-helmet";
 
-export default class Index extends React.Component {
-  render() {
-    return (
-      <div className="page-content">
-        <Helmet>
-          <title>Hello World</title>
-        </Helmet>
-        <h1>Hello World</h1>
+const IndexPage = () => {
+
+  const homepage = { title: "Hello World", content: "Awesome place here!"}
+
+  return (
+    <div className="page-content">
+      <Helmet>
+        <title>{ homepage.title }</title>
+      </Helmet>
+      <h1 dangerouslySetInnerHTML={{ __html: homepage.title }}></h1>
+      <div className="content">
+        <div className="inner" dangerouslySetInnerHTML={{ __html: homepage.content }} />
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+export default IndexPage;
+
